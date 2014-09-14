e915576030dbf6298ff37dd4830f8da3749abad8
Dirk Jäckel <github@dirk.jaeckel.name>
2014-09-14 16:15:09 +0200

    Bidirectional messaging.

--- OkBitcoin/mobile/src/main/java/org/diskordia/okbitcoin/MyActivity.java
+++ OkBitcoin/mobile/src/main/java/org/diskordia/okbitcoin/MyActivity.java
@@ -2,21 +2,24 @@ package org.diskordia.okbitcoin;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MyActivity extends Activity {
+    public static final String TAG = "OkBitcoin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
         setContentView(R.layout.activity_my);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        Log.d(TAG, "onCreateOptionsMenu()");
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
@@ -24,6 +27,8 @@ public class MyActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.d(TAG, "onOptionsItemSelected()");
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -34,3 +39,4 @@ public class MyActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+
